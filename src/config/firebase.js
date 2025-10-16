db87bf35ef550c9919898949deeda331a8f2b505
enricobrunazzo <89249309+enricobrunazzo@users.noreply.github.com>
2025-10-16 16:02:54 +0200

    Confirm Firebase Realtime Database URL configuration

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -6,7 +6,7 @@ import { getDatabase } from "firebase/database";
 const firebaseConfig = {
   apiKey: "AIzaSyC1Dw0zFBOYwW6uVfEa0zHC5YBOFUhHsmI",
   authDomain: "vigontina-stats.firebaseapp.com",
-  databaseURL: "https://vigontina-stats-default-rtdb.europe-west1.firebasedatabase.app",
+  databaseURL: "https://vigontina-stats-default-rtdb.europe-west1.firebasedatabase.app/",
   projectId: "vigontina-stats",
   storageBucket: "vigontina-stats.firebasestorage.app",
   messagingSenderId: "979551248607",
@@ -21,5 +21,10 @@ export const db = getFirestore(app);
 
 // Inizializza Realtime Database (per partite attive)
 export const realtimeDb = getDatabase(app);
+
+// Debug: verifica connessione
+if (typeof window !== 'undefined') {
+  console.log('Firebase Realtime Database configurato:', firebaseConfig.databaseURL);
+}
 
 export default app;
